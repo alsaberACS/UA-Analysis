b3fffdff5497b535b4e4dbffa31e423b032d0577
alsaberACS <admin@acs-kw.com>
2018-09-30 19:23:51 +0100

    UA ver2

--- UA Analysis.R
+++ UA Analysis.R
@@ -1,3 +1,7 @@
+#----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>")
 #---------------Libraries-------------- (Step 1)
 library(finalfit)
 library(Hmisc)
@@ -26,7 +30,6 @@ library(foreign)
 #---------------Loading Data---------- (Step 2)
 UA <- read.spss("UAD Study Corrected.sav", use.value.label=TRUE, to.data.frame=TRUE)
 #---------------Coding Baseline factors---------- (Step 3)
-
 UA$RF1 <- ifelse(UA$Record == "Baseline", UA$RF, NA)
 UA$DOD.years1 <- ifelse(UA$Record == "Baseline", UA$DOD.years, NA)
 UA$AGE.years1 <- ifelse(UA$Record == "Baseline", UA$AGE.years, NA)
@@ -39,7 +42,6 @@ UA$SICCA1 <- ifelse(UA$Record == "Baseline", UA$SICCA, NA)
 UA$Smoking1 <- ifelse(UA$Record == "Baseline", UA$Smoking, NA)
 UA$Family_History1 <- ifelse(UA$Record == "Baseline", UA$Family_History, NA)
 UA$Nationality1 <- ifelse(UA$Record == "Baseline", UA$Nationality, NA)
-
 
 UA$RF1 <- factor(UA$RF1,
           levels = c(1,2),
@@ -72,18 +74,77 @@ UA$Nationality1 <- factor(UA$Nationality1,
                              levels = c(1,2),
                              labels = c("Kuwaiti's", "non-Kuwaiti's"))
 
-#---------------GLM Model---------- (Step 3)
+#---------------summary_factorlist for DAS28 Levels---------- (Step 3)
 explanatory = c("VAS","ESR", "CRP", "HAQ", "Patient.s.Global.Assessment", "Physician.s.Global.Assessment",
-                "SDAI", "CDAI", "Uric.acid", "DOD.years1", "AGE.years1", "Tender", "Swollen", "Gender1", 
+                "SDAI", "CDAI", "Uric.acid","DAS28", "DOD.years1", "AGE.years1", "Tender", "Swollen", "Gender1", 
                 "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1", "Family_History1", "Asthma", 
-                "Nationality1", "onbio", "Bio_Mono", "DM", "CAD", "Hyperlipidemia", "Hypertension")
+                "Nationality1", "onbio", "Bio_Mono", "DM", "CAD", "Hyperlipidemia", "Hypertension", "UA_Group")
+
+explanatory_multi = c("DOD.years1", "Gender1", "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1")
 dependent = "DAS28_Group"
+Table1 <- summary_factorlist(UA, dependent, explanatory, p=TRUE, add_dependent_label=TRUE)
+#knitr::kable(Table1)
+#---------------summary_factorlist for UA Levels---------- (Step 4)
+explanatory = c("VAS","ESR", "CRP", "HAQ", "Patient.s.Global.Assessment", "Physician.s.Global.Assessment",
+                "SDAI", "CDAI", "Uric.acid", "DAS28", "DOD.years1", "AGE.years1", "Tender", "Swollen", "Gender1", 
+                "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1", "Family_History1", "Asthma", 
+                "Nationality1", "onbio", "Bio_Mono", "DM", "CAD", "Hyperlipidemia", "Hypertension", "DAS28_Group")
 
-  Table1 <- summary_factorlist(UA, dependent, explanatory, p=TRUE)
-  Table2 <- summary_factorlist(UA, dependent, explanatory, p=TRUE)
-
-#---------------GLM Model---------- (Step 3)
+explanatory_multi = c("DOD.years1", "Gender1", "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1")
+dependent = "UA_Group"
+  Table2 <- summary_factorlist(UA, dependent, explanatory, p=TRUE, add_dependent_label=TRUE)
+  #knitr::kable(Table2)
+#---------------GLM Model for DAS28 Levels---------- (Step 5)
 #glm(depdendent3 ~ explanatory3, family="binomial")
-
-  Table3 <- finalfit(UA, dependent, explanatory, na_include=TRUE)
-  Table4 <- finalfit(UA,dependent, explanatory, p=TRUE)
+  explanatory = c("VAS","ESR", "CRP", "HAQ", "Patient.s.Global.Assessment", "Physician.s.Global.Assessment",
+                  "SDAI", "CDAI", "Uric.acid", "DOD.years1", "AGE.years1", "Tender", "Swollen", "Gender1", 
+                  "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1", "Family_History1", "Asthma", 
+                  "Nationality1", "onbio", "Bio_Mono", "DM", "CAD", "Hyperlipidemia", "Hypertension", "UA_Group")
+  
+  explanatory_multi = c("DOD.years1", "Gender1", "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1")
+  dependent = "DAS28_Group"
+  Table3 <- finalfit(UA, dependent, explanatory, explanatory_multi, na_include=TRUE, add_dependent_label=TRUE)
+  #knitr::kable(Table3)
+  #---------------GLM Model for UA Levels---------- (Step 6)
+  explanatory = c("VAS","ESR", "CRP", "HAQ", "Patient.s.Global.Assessment", "Physician.s.Global.Assessment",
+                  "SDAI", "CDAI", "DAS28", "DOD.years1", "AGE.years1", "Tender", "Swollen", "Gender1", 
+                  "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1", "Family_History1", "Asthma", 
+                  "Nationality1", "onbio", "Bio_Mono", "DM", "CAD", "Hyperlipidemia", "Hypertension", "DAS28_Group")
+  
+  explanatory_multi = c("DOD.years1", "Gender1", "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1")
+  dependent = "UA_Group"
+  Table4 <- finalfit(UA, dependent, explanatory, explanatory_multi, p=TRUE, add_dependent_label=TRUE)
+ # knitr::kable(Table4)
+  #---------------Odds ratio for DAS28 Levels---------- (Step 7)
+  explanatory = c("DOD.years1", "Gender1",
+                  "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1",  
+                   "DM", "CAD", "UA_Group")
+  dependent = "DAS28_Group"
+  or_plot(UA, dependent, explanatory)
+  #---------------Random Effect for DAS28 Levels---------- (Step 8)
+  explanatory = c("VAS","ESR", "CRP", "HAQ", "Patient.s.Global.Assessment", "Physician.s.Global.Assessment",
+                  "SDAI", "CDAI", "Uric.acid", "DOD.years1", "AGE.years1", "Tender", "Swollen", "Gender1", 
+                  "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1", "Family_History1", "Asthma", 
+                  "Nationality1", "onbio", "Bio_Mono", "DM", "CAD", "Hyperlipidemia", "Hypertension", "UA_Group")
+  random_effect = "Hospital"
+  explanatory_multi = c("DOD.years1", "Gender1", "Current1", "RF1", "CCP1", "ANA1", "Nodules1", "SICCA1", "Smoking1")
+  dependent = "DAS28_Group"
+  
+  # Separate tables
+  UA %>%
+    summary_factorlist(dependent,
+                       explanatory, fit_id=TRUE) -> Table5
+  
+  UA %>%
+    glmuni(dependent, explanatory) %>%
+    fit2df(estimate_suffix=" (univariable)") -> Table6
+  
+  
+  
+  # Pipe together
+  Table5 %>%
+    finalfit_merge(Table6) %>%
+    
+    
+    select(-c(fit_id, index)) %>% # remove unnecessary columns
+    dependent_label(colon_s, dependent, prefix="") -> Table9 # place dependent variable label
